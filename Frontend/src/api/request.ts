01eb3ac3c9f02c3e588af73d7dbe25ad6f176a2d
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-12-12 22:06:23 +0100

    Containerize frontend application

--- Frontend/src/api/request.ts
+++ Frontend/src/api/request.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api/';
+const API_BASE = 'http://localhost/api/';
 
 export const Delete = async(url: string) => {
   return await fetch(`${API_BASE}${url}`, {
