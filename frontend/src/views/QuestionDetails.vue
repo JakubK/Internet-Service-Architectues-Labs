af9f3017f675768c727c614abb7f963dc999ec55
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-01-09 00:19:43 +0100

    Display question image in details view

--- frontend/src/views/QuestionDetails.vue
+++ frontend/src/views/QuestionDetails.vue
@@ -40,56 +40,58 @@ const deleteAnswer = async (id: number) => {
 </script>
 
 <template>
-  <router-link v-if="readOnly" :to="{ path: '/answer/new', query: { id: route.query.id } }">
-    <button>Add new Answer</button>
-  </router-link>
-  <table class="category-details">
-    <tr>
-      <th>Id</th>
-      <td>{{ route.query.id }}</td>
-    </tr>
-    <tr>
-      <th>Content</th>
-      <td v-if="readOnly">{{ question.content }}</td>
-      <td v-else>
-        <input type="text" v-model="question.content" />
-      </td>
-    </tr>
-    <tr>
-      <th>Is multiselectable?</th>
-      <td v-if="readOnly">{{ question.multiSelect }}</td>
-      <td v-else>
-        <input type="checkbox" v-model="question.multiSelect" />
-      </td>
-    </tr>
-  </table>
-
-  <button v-if="!readOnly" @click="applyChanges()">Apply changes</button>
-  <div v-else>
-    <hr />
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Text</th>
-          <td>Actions</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="ans in answers" :key="ans.id">
-          <td>{{ ans.id }}</td>
-          <td>{{ ans.text }}</td>
-          <td>
-            <router-link :to="{ path: '/answer', query: { id: ans.id } }">
-              <button>Details</button>
-            </router-link>
-            <router-link :to="{ path: '/answer/edit', query: { id: ans.id } }">
-              <button>Edit</button>
-            </router-link>
-            <button @click="deleteAnswer(ans.id)">Delete</button>
-          </td>
-        </tr>
-      </tbody>
+  <div>
+    <router-link v-if="readOnly" :to="{ path: '/answer/new', query: { id: route.query.id } }">
+      <button>Add new Answer</button>
+    </router-link>
+    <img :src="'http://localhost/api/files/download/' + route.query.id"/>
+    <table class="category-details">
+      <tr>
+        <th>Id</th>
+        <td>{{ route.query.id }}</td>
+      </tr>
+      <tr>
+        <th>Content</th>
+        <td v-if="readOnly">{{ question.content }}</td>
+        <td v-else>
+          <input type="text" v-model="question.content" />
+        </td>
+      </tr>
+      <tr>
+        <th>Is multiselectable?</th>
+        <td v-if="readOnly">{{ question.multiSelect }}</td>
+        <td v-else>
+          <input type="checkbox" v-model="question.multiSelect" />
+        </td>
+      </tr>
     </table>
+    <button v-if="!readOnly" @click="applyChanges()">Apply changes</button>
+    <div v-else>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Text</th>
+            <td>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="ans in answers" :key="ans.id">
+            <td>{{ ans.id }}</td>
+            <td>{{ ans.text }}</td>
+            <td>
+              <router-link :to="{ path: '/answer', query: { id: ans.id } }">
+                <button>Details</button>
+              </router-link>
+              <router-link :to="{ path: '/answer/edit', query: { id: ans.id } }">
+                <button>Edit</button>
+              </router-link>
+              <button @click="deleteAnswer(ans.id)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
